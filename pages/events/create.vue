31d51db416897a6348dd9f7056c1d1455401a262
Álvaro Márquez <marquezalvaro13@gmail.com>
2020-06-29 11:53:32 +0200

    Add components and vuex events

---
+++ pages/events/create.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Create An Event</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Event Create',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Where you can create a new event in your neighborhood'
+        }
+      ]
+    }
+  }
+}
+</script>
